ab22a0c3861edef90670afea6a1c0d37fbce4b0f
Mihail <kto-kuda-a-ti-suda@yandex.ru>
2026-02-24 23:53:11 +0300

    spring cloud added v4 corrections

--- core/main-service/src/main/java/ru/yandex/practicum/MainServiceApplication.java
+++ core/main-service/src/main/java/ru/yandex/practicum/MainServiceApplication.java
@@ -1,4 +1,4 @@
-package ru.practicum;
+package ru.yandex.practicum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,8 +8,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @ConfigurationPropertiesScan
 @EnableDiscoveryClient
-public class StatsServer {
+public class MainServiceApplication {
     public static void main(String[] args) {
-        SpringApplication.run(StatsServer.class, args);
+        SpringApplication.run(MainServiceApplication.class, args);
     }
 }
