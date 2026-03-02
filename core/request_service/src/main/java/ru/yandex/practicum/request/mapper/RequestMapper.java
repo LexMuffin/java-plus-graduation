92965a7d281913bbd6405c41b1aeef6a80bd97f9
Mihail <kto-kuda-a-ti-suda@yandex.ru>
2026-03-03 01:24:48 +0300

    microservices v7 minor correction v2

--- core/request_service/src/main/java/ru/yandex/practicum/request/mapper/RequestMapper.java
+++ core/request_service/src/main/java/ru/yandex/practicum/request/mapper/RequestMapper.java
@@ -2,14 +2,8 @@ package ru.yandex.practicum.request.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.Named;
-import ru.yandex.practicum.dto.event.EventFullDto;
 import ru.yandex.practicum.dto.request.ParticipationRequestDto;
-import ru.yandex.practicum.dto.user.UserDto;
-import ru.yandex.practicum.dto.user.UserShortDto;
-import ru.yandex.practicum.event.model.Event;
 import ru.yandex.practicum.request.model.ParticipationRequest;
-import ru.yandex.practicum.user.model.User;
 
 import java.util.List;
 
@@ -21,22 +15,4 @@ public interface RequestMapper {
     ParticipationRequestDto toDto(ParticipationRequest request);
 
     List<ParticipationRequestDto> toDto(List<ParticipationRequest> requests);
-
-    @Mapping(target = "id", ignore = true)
-    User toUser(UserDto userDto);
-
-    @Mapping(target = "initiator", source = "initiator", qualifiedByName = "mapUserShortDtoToLong")
-    @Mapping(target = "category", source = "category.id")
-    @Mapping(target = "confirmedRequests", ignore = true)
-    @Mapping(target = "createdOn", ignore = true)
-    @Mapping(target = "publishedOn", ignore = true)
-    @Mapping(target = "views", ignore = true)
-    @Mapping(target = "location", ignore = true)
-    Event toEvent(EventFullDto eventDto);
-
-    @Named("mapUserShortDtoToLong")
-    default Long mapUserShortDtoToLong(UserShortDto userShortDto) {
-        if (userShortDto == null) return null;
-        return userShortDto.getId();
-    }
 }
