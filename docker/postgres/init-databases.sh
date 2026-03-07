14d6151bdce61bf9e504bee75f939203717a24b6
LexMuffin <kto-kuda-a-ti-suda@yandex.ru>
2026-03-07 18:05:20 +0300

    microservices v83 minor category-service fix test v27

--- docker/postgres/init-databases.sh
+++ docker/postgres/init-databases.sh
@@ -10,7 +10,7 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
     GRANT ALL PRIVILEGES ON DATABASE user_service TO "$POSTGRES_USER";
     GRANT ALL PRIVILEGES ON DATABASE event_service TO "$POSTGRES_USER";
     GRANT ALL PRIVILEGES ON DATABASE request_service TO "$POSTGRES_USER";
-    GRANT ALL PRIVILEGES ON DATABASE comment_service TO "$POSTGRES_USER";
+    GRANT ALL PRIVILEGES ON DATABASE category_service TO "$POSTGRES_USER";
 EOSQL
 
 echo "All databases created successfully"
