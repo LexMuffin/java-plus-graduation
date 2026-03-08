bfe7926df413da3735233573843770e5bd2bf814
LexMuffin <kto-kuda-a-ti-suda@yandex.ru>
2026-03-08 08:54:47 +0300

    microservices v119 stats-client test

--- stats/stats-server/src/main/java/ru/yandex/practicum/StatsServer.java
+++ stats/stats-server/src/main/java/ru/yandex/practicum/StatsServer.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@ComponentScan
 public class StatsServer {
     public static void main(String[] args) {
         SpringApplication.run(StatsServer.class, args);
