33ec48ab1d18eb3c3fe5dfa7232cc14afa7e1fc8
LexMuffin <kto-kuda-a-ti-suda@yandex.ru>
2026-03-12 10:28:57 +0300

    recommendations v11

--- stats/aggregator/src/main/java/ru/yandex/practicum/AggregatorApp.java
+++ stats/aggregator/src/main/java/ru/yandex/practicum/AggregatorApp.java
@@ -4,17 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.ConfigurableApplicationContext;
-import ru.yandex.practicum.starter.AggregationStarter;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @ConfigurationPropertiesScan
 public class AggregatorApp {
     public static void main(String[] args) {
-        ConfigurableApplicationContext context = SpringApplication.run(AggregatorApp.class, args);
-
-        AggregationStarter aggregator = context.getBean(AggregationStarter.class);
-        aggregator.start();
+        SpringApplication.run(AggregatorApp.class, args);
     }
 }
